1a4b6e92abbb208fa5ca57c21ebe158f7b33b01b
viv-dan <81843793+viv-dan@users.noreply.github.com>
2024-01-29 17:17:03 -0500

    class activity 012924

--- quiz/scripts/addEvent.js
+++ quiz/scripts/addEvent.js
@@ -1,18 +1,20 @@
-window.onload = function() {
-    let x = document.getElementById('myBtn');
-    x.addEventListener('mouseover', myFunction('Moused over!'));
-    x.addEventListener('click', mySecondFunction('Clicked!'));
-    x.addEventListener('mouseout', myThirdFunction('Moused out!'));
+window.onload = function () {
+  let x = document.getElementById('myBtn')
+  x.addEventListener('mouseover', myFunction('Moused over!'))
+  x.addEventListener('click', mySecondFunction('Clicked!'))
+  x.addEventListener('mouseout', myThirdFunction('Moused out!'))
 }
 
 function myFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+  return () => {
+    document.getElementById('demo').textContent = msg
+  }
 }
 
 function mySecondFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+  return () => (document.getElementById('demo').textContent = msg)
 }
 
 function myThirdFunction(msg) {
-    document.getElementById('demo').textContent = msg;
+  return () => (document.getElementById('demo').textContent = msg)
 }
--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -1,31 +1,50 @@
-window.onload = function() {
-  document.getElementById('addTableBtn').addEventListener('click', addTable);
+window.onload = function () {
+  document.getElementById('addTableBtn').addEventListener('click', addTable)
 }
 
 function createTRNode(colNodes) {
-  let trNode = document.createElement("tr");
-  colNodes.forEach(function(colNode) {
-    trNode.appendChild(colNode);
+  let trNode = document.createElement('tr')
+  colNodes.forEach(function (colNode) {
+    trNode.appendChild(colNode)
   })
-  return trNode;
+  return trNode
 }
 
 function createTDNode(childNode) {
-  let tdNode = document.createElement("td");
-  tdNode.appendChild(childNode);
-  return tdNode;
+  let tdNode = document.createElement('td')
+  tdNode.appendChild(childNode)
+  return tdNode
 }
 
 function createTxtNode(txt) {
-  let txtNode = document.createTextNode(txt);
-  return txtNode;
+  let txtNode = document.createTextNode(txt)
+  return txtNode
+}
+
+function createBtnNode(text, event, fun) {
+  let btnNode = document.createElement('button')
+  btnNode.textContent = text
+  btnNode.addEventListener(event, fun)
+  return btnNode
 }
 
 function addTable() {
-  const tableNode = document.createElement("table");
-  for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+  const tableNode = document.createElement('table')
+  for (let i = 0; i < 3; i++) {
+    let col1 = createTDNode(createTxtNode('Cell (' + i + ', 0)'))
+    let col2 = createTDNode(
+      createBtnNode('Edit text', 'click', function () {
+        edit(col1)
+      })
+    )
+    tableNode.appendChild(createTRNode([col1, col2]))
   }
-  document.getElementById("root").appendChild(tableNode);
+  document.getElementById('root').appendChild(tableNode)
+}
+
+function edit(node) {
+  let newnode = document.createElement('input')
+  newnode.type = 'text'
+  newnode.placeholder = 'Enter cell (x,y) ...'
+  node.replaceChild(newnode, node.childNodes[0])
 }
