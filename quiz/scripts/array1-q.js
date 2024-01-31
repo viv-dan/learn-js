4a04bde38541230949448baf4eab4eb7f1610bd0
viv-dan <81843793+viv-dan@users.noreply.github.com>
2024-01-31 16:21:45 -0500

    class activity on 013124

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -1,32 +1,39 @@
-window.onload = function() {
-    const list = document.getElementById('Top5');
-    const inp = document.getElementById('inp');
-    const btn = document.getElementById('btn');
-    list.innerHTML = '';
+window.onload = function () {
+  const list = document.getElementById('Top5')
+  const inp = document.getElementById('inp')
+  const btn = document.getElementById('btn')
+  list.innerHTML = ''
 
-    const myHistory = [];
-    const MAX_HISTORY = 5;
+  const myHistory = []
+  const MAX_HISTORY = 5
 
-    btn.onclick = () => {
+  btn.onclick = () => {
     // we will only allow a term to be entered if the search input isn't empty
-      if (inp.value !== '') {
-              // empty the list so that we don't display duplicate entries
-              // the display is regenerated every time a search term is entered.
-              list.innerHTML = '';
+    if (inp.value !== '') {
+      myHistory.unshift(inp.value)
+      // empty the list so that we don't display duplicate entries
+      // the display is regenerated every time a search term is entered.
+      list.innerHTML = ''
+      myHistoryCopy = [...myHistory]
+      myHistoryCopy.sort((a, b) => {
+        return a.length - b.length
+      })
 
-              // loop through the sorted array, and display all the search terms in the list
-              for (const itemText of myHistoryCopy) {
-                
-              }
+      // loop through the sorted array, and display all the search terms in the list
+      for (const itemText of myHistoryCopy) {
+        let item = document.createElement('li')
+        item.textContent = itemText
+        list.appendChild(item)
+      }
 
-              // If the array length is 5 or more, remove the oldest search term
-              if (myHistory.length >= MAX_HISTORY) {
-                
-              }
+      // If the array length is 5 or more, remove the oldest search term
+      if (myHistory.length >= MAX_HISTORY) {
+        myHistory.shift()
+      }
 
-              // empty the search input and focus it, ready for the next term to be entered
-              inp.value = '';
-              btn.focus();
-          }
+      // empty the search input and focus it, ready for the next term to be entered
+      inp.value = ''
+      btn.focus()
     }
+  }
 }
--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -1,11 +1,24 @@
 /* anonymizes the username(local part) of an email ID, that is, 
 replace the username part with * characters.
 */
-const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
+const emails = [
+  'mary@northeastern.edu',
+  'ari@khoury.northeastern.edu',
+  'jk_neu.edu',
+  'jk@neu.edu',
+  'jsned@',
+  'ai_me@mugar.northeastern.edu',
+]
 window.onload = function hideEmail() {
-    const list = document.getElementById('emails')
-    list.innerHTML = '';
-    for (const email of emails) {
-        // complete the loop
+  const list = document.getElementById('emails')
+  list.innerHTML = ''
+  for (const email of emails) {
+    if (email.match(/^[a-z0-9_]+@(.+)*northeastern.edu$/)) {
+      const listitem = document.createElement('li')
+      listitem.textContent =
+        email.slice(0, email.indexOf('@')).replace(/./g, '*') +
+        email.slice(email.indexOf('@'))
+      list.appendChild(listitem)
     }
+  }
 }
